f8beb5cc17635ea4ead27e9bb095663c44003329
Daniel Sanchez <danielsanchez.snez@gmail.com>
2022-11-06 10:28:33 -0800

    Print rock, paper or scissors at random

--- javascript.js
+++ javascript.js
@@ -2,6 +2,14 @@ console.log(getComputerChoice())
 
 
 function getComputerChoice(){
-    return Math.floor(Math.random() * 3)+1;
+    let choice = Math.floor(Math.random() * 3)+1
+    if (choice == 1){
+        return "Rock"
+    } else if (choice == 2) {
+        return "Paper"
+    }
+    else {
+        return "Scissors"
+    }
 }
 
