31b519c6d3dd2ea41dee1e4ec13c9b94d6ad2998
Daniel Sanchez <danielsanchez.snez@gmail.com>
2022-11-06 11:06:36 -0800

    Add best of 5 game

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
 //console.log(getComputerChoice())
-const playerSelection="SCIssoRs"
-const computerSelection=getComputerChoice()
+let playerSelection="SCIssoRs"
+let computerSelection=getComputerChoice()
 
-console.log(playRound(playerSelection, computerSelection))
+//console.log(playRound(playerSelection, computerSelection))
+game()
 
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3)+1
@@ -35,11 +36,11 @@ function playRound(playerSelection, computerSelection){
         } else if (computerSelection=="paper"){
             result = "Draw!"
         } else{
-            result = "You Lose! Scissors beat paper"
+            result = "You lose! Scissors beat paper"
         }
     } else if (playerSelection=="scissors"){
         if (computerSelection=="rock"){
-            result = "You Lose! Rock beats scissors"
+            result = "You lose! Rock beats scissors"
         } else if (computerSelection=="paper"){
             result = "You win! Scissors beats paper"
         } else{
@@ -48,4 +49,30 @@ function playRound(playerSelection, computerSelection){
     }
 
     return result
+}
+
+function game(){
+    let humanScore = 0
+    let computerScore = 0
+    for (let i = 0; i < 5; i++){
+        playerSelection = prompt("Choose your weapon!")
+        computerSelection=getComputerChoice()
+        result = playRound(playerSelection, computerSelection)
+        if (result.includes("win")==true){
+            humanScore++
+        } else if (result.includes("lose")==true){
+            computerScore++
+        } else {
+            i--
+        }
+        console.log(result)
+    }
+
+    if (humanScore>computerScore){
+        console.log("Human wins!")
+    } else if (humanScore<computerSelection){
+        console.log("Computer wins!")
+    } else {
+        console.log("Draw!")
+    }
 }
