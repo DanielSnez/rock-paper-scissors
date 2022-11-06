f797e90eb479e1cdec88e5a4e3e9a45b8a2c8c75
Daniel Sanchez <danielsanchez.snez@gmail.com>
2022-11-06 10:49:54 -0800

    Modify function to return winner of 1 game

--- javascript.js
+++ javascript.js
@@ -1,15 +1,51 @@
-console.log(getComputerChoice())
+//console.log(getComputerChoice())
+const playerSelection="SCIssoRs"
+const computerSelection=getComputerChoice()
 
+console.log(playRound(playerSelection, computerSelection))
 
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3)+1
     if (choice == 1){
-        return "Rock"
+        return "rock"
     } else if (choice == 2) {
-        return "Paper"
+        return "paper"
     }
     else {
-        return "Scissors"
+        return "scissors"
     }
 }
 
+function playRound(playerSelection, computerSelection){
+    playerSelection=playerSelection.toLowerCase()
+    console.log(playerSelection)
+    console.log(computerSelection)
+    
+    if (playerSelection=="rock"){
+        if (computerSelection=="rock"){
+            result = "Draw!"
+        } else if (computerSelection=="paper"){
+            result = "You lose! Paper beats rock"
+        } else{
+            result = "You win! Rock beats scissors"
+        }
+    } else if (playerSelection=="paper"){
+        if (computerSelection=="rock"){
+            result = "You win! Paper beats rock"
+        } else if (computerSelection=="paper"){
+            result = "Draw!"
+        } else{
+            result = "You Lose! Scissors beat paper"
+        }
+    } else if (playerSelection=="scissors"){
+        if (computerSelection=="rock"){
+            result = "You Lose! Rock beats scissors"
+        } else if (computerSelection=="paper"){
+            result = "You win! Scissors beats paper"
+        } else{
+            result = "Draw!"
+        }        
+    }
+
+    return result
+}
